ec987a0613366db821410f65ab522263c8bb4996
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-10-03 15:17:05 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,10 @@ var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 var newName1 = femaleNames.push('Monika');
 var newName2 = maleNames.push('Marcin');
-allNames.indexOf(newName1) === -1
-allNames.indexOf(newName2) === -1
-var newName1 = allNames.push('Monika');
-var newName2 = allNames.push('Marcin');
+	if allNames.indexOf(newName1) === -1 {
+		var newName1 = allNames.push('Monika');
+	}
+	if allNames.indexOf(newName2) === -1 {
+		var newName2 = allNames.push('Marcin');
+	}
 console.log( allNames );
